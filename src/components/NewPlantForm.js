883435948583dcb2366d9817ca27aa7de0c66ad9
Rochelle Awuor <awuorochelle75@gmail.com>
2025-04-27 14:18:29 +0300

    modifies files

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,6 +1,6 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-function NewPlantForm({setPlants}) {
+function NewPlantForm({ setPlants }) {
   const [name, setName] = useState("");
   const [image, setImage] = useState("");
   const [price, setPrice] = useState("");
@@ -11,36 +11,56 @@ function NewPlantForm({setPlants}) {
     const newPlant = {
       name,
       image,
-      price: parseFloat(price),
+      price, 
     };
 
-    
-     fetch("http://localhost:6001/plants", {
+    fetch("http://localhost:6001/plants", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "Application/JSON",
       },
       body: JSON.stringify(newPlant),
     })
       .then((r) => r.json())
       .then((newPlantFromServer) => {
-     
         setPlants((prevPlants) => [...prevPlants, newPlantFromServer]);
-
         
+       
         setName("");
         setImage("");
-        setPrice("");
+        setPrice(""); 
       });
   }
 
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form onSubmit = {handleSubmit}>
-        <input type="text" name="name" placeholder="Plant name" value={name} onChange={(e) => setName(e.target.value)} required />
-        <input type="text" name="image" placeholder="Image URL" value={image} onChange={(e) => setImage(e.target.value)} required />
-        <input type="number" name="price" step="0.01" placeholder="Price" value={price} onChange={(e) => setPrice(e.target.value)} required />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Plant name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          name="image"
+          placeholder="Image URL"
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
+          required
+        />
+        <input
+          type="number"
+          name="price"
+          step="0.01"
+          placeholder="Price"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+          required
+        />
         <button type="submit">Add Plant</button>
       </form>
     </div>
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,14 +1,16 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList({plants}) {
+
+function PlantList({ plants, setPlants }) {
   return (
-    <ul className="cards">{/* render PlantCards components in here */}
-    {plants.map((plant)=> (
-      <PlantCard key={plant.id} plant={plant} />
-    ))}
+    <ul className="cards">
+      {plants.map((plant) => (
+        <PlantCard key={plant.id} plant={plant} setPlants={setPlants} />
+      ))}
     </ul>
   );
 }
+
 
 export default PlantList;
